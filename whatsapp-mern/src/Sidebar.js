5ec750859a0bdedf58b35f47623d37c5ba051d4f
mauro Leos <mauroleos94@gmail.com>
2021-03-03 08:07:00 -0600

    Added material UI icons

--- whatsapp-mern/src/Sidebar.js
+++ whatsapp-mern/src/Sidebar.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import { Avatar, IconButton } from "@material-ui/core"
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>I am the Sidebar component</h1>
+            <div className="sidebar__header">
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                </div>
+                <div></div>
+
+            </div>
         </div>
     );
 }
