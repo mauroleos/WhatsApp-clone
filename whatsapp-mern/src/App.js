a742b1b24b550dca8f3be12b33f3f194d475a835
mauro Leos <mauroleos94@gmail.com>
2021-03-02 22:39:46 -0600

    Added sidebar and chat components

--- whatsapp-mern/src/App.js
+++ whatsapp-mern/src/App.js
@@ -6,8 +6,10 @@ import Chat from "./Chat"
 function App() {
   return (
     <div className="app">
+      <div className = "app__body">
       <Sidebar />
       <Chat />
+      </div>
     </div>
   );
 }
