54d7430d77a16cd730ef3077026881e5ce6bbc86
mauro Leos <mauroleos94@gmail.com>
2021-03-06 16:14:27 -0600

    Worked on chat box

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -1,4 +1,7 @@
-import { Avatar } from "@material-ui/core";
+import { Avatar, IconButton } from "@material-ui/core";
+import Search from "@material-ui/icons/Search";
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import React from "react";
 import "./Chat.css"
 
@@ -7,6 +10,31 @@ function Chat() {
         <div className="chat">
             <div className="chat__header">
                 <Avatar />
+
+                <div className="chat__headerInfo">
+                    <h3>Room Name</h3>
+                    <p>Last seen at...</p>
+                </div>
+
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <Search />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFileIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            
+            <div className="chat__body">
+                <p>
+                    <span className="chat__name">Mauro</span>
+                    This is a message
+                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                </p>
             </div>
         </div>
     )
