f42d842900d689c8440a9f0a6366fa1a60bc8a18
mauro Leos <mauroleos94@gmail.com>
2021-03-05 20:55:13 -0600

    Added to CSS and created search bar

--- whatsapp-mern/src/Sidebar.js
+++ whatsapp-mern/src/Sidebar.js
@@ -2,14 +2,23 @@ import React from "react";
 import "./Sidebar.css";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import { Avatar, IconButton } from "@material-ui/core"
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
+                <Avatar src="https://avatars.githubusercontent.com/u/60679353?s=400&u=2ddc3227d206dd164c2e4c8a66c46a68eea5f9d5&v=4"/>
                 <div className="sidebar__headerRight">
                     <IconButton>
                         <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
                     </IconButton>
                 </div>
                 <div></div>
