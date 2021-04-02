22240eedf14b3101438648c11351413ff23bc9af
mauro Leos <mauroleos94@gmail.com>
2021-04-02 08:11:12 -0500

    Added firebase db

--- whatsapp-mern/src/firebase.js
+++ whatsapp-mern/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyD0Rwcz0EyIvIh7RyntcNVadei9N5eiSTo",
     authDomain: "whatsapp-mern-c89c1.firebaseapp.com",
@@ -6,3 +8,15 @@ const firebaseConfig = {
     messagingSenderId: "601842788932",
     appId: "1:601842788932:web:6558ff5f61a5813a0a2a11"
   };
+
+  // Initialize firebase
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  // Gets our Database
+  const db = firebaseApp.firestore();
+  // Authentication 
+  const auth = firebase.auth();
+  // Google's Authentication
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider };
+  export default db;
